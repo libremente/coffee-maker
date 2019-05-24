ebd0d46f3a6905c36865a17b8d3d778c9527f47e
libremente <surf@libremente.eu>
2019-05-24 13:27:19 +0200

    Added source code

---
+++ coffee.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  
+  printf("[0] Prendi la macchina del caffè\n");
+  printf("[1] Metti la macchina sul gas\n");
+  printf("[2] Aprila\n");
+  printf("[3] Metti l'acqua nella caldaia\n");
+  printf("[4] Metti il caffè nel portafiltro \n");
+  printf("[5] Chiudi il tutto\n");
+  printf("[6] Accendi il gas\n");
+
+  return 0;
+}
